3f56f4e99586a56d554efb1fafef41906dec027e
冯勇 <12780822+lionfeng@user.noreply.gitee.com>
2023-05-17 18:15:12 +0800

    feat

--- index.js
+++ index.js
@@ -3,31 +3,40 @@ const prettier = require("prettier");
 const chokidar = require('chokidar');
 const _ = require('lodash')
 
-let oldFile = '';
+const FileCache = {};
 
-function VitePrettier(option) {
+function VitePrettier(option, watchOptions={}) {
+    let { watchDirs=[], ignoreDirs=[] } = watchOptions;
+    watchDirs = ['vite.config.js', 'vite.config.ts', 'src/']
+    let FSWatcher
     return {
         name: 'vite-plugin-prettier',
+        enforce: 'pre',
         apply: 'serve', // 只在开发阶段执行
-        configResolved() {
-            let FSWatcher = chokidar
-                .watch('.', {
-                    ignored: ['**/node_modules/**', '**/.git/**'],
+        configureServer(server){
+            server.httpServer.on('close', function(){
+                FSWatcher.close()
+            })
+        },
+        config() {
+            FSWatcher = chokidar
+                .watch(watchDirs, {
+                    ignored: ignoreDirs,
                     ignoreInitial: true,
                 })
                 .on('change', _.debounce(path => {
-                    if (path == 'vite.config.js') {
-                        FSWatcher.close()
-                    };
+                    FileCache[path] = FileCache[path] || {};
                     let fileStr = fs.readFileSync(path, 'utf8');
-                    if (fileStr == oldFile) return;
+                    if (fileStr == FileCache[path].file || FileCache[path].parsing) return;
+                    FileCache[path].parsing = true;
                     const prettierSource = prettier.format(fileStr, {
                         filepath: path,
                         ...option,
                     });
                     fs.writeFile(path, prettierSource, 'utf8', (err) => {
                         if (err) { return console.log(err); }
-                        oldFile = prettierSource;
+                        FileCache[path].parsing = false;
+                        FileCache[path].file = prettierSource;
                     });
                 }, 200));
         },
